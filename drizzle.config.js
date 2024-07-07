5464a55837fa7235465717dd920f66531e1c7ec7
Nihal Ahmad <nehalhmd14@gmail.com>
2024-07-07 12:04:30 +0530

    added public api in env file

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,8 +3,7 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://SASS%20AI-Content-Generator_owner:xigqbWB4j1ZG@ep-wispy-fire-a5ycwhbh.us-east-2.aws.neon.tech/AiContentGenerator?sslmode=require',
-
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
     }
   };
   
